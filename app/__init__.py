56bdb1986e12b7a62b311d7a5d52fd331de65d0d
yashash mathur <yashashmathur2005@gmail.com>
2026-02-19 13:28:39 +0530

    Fix: Enable CORS for cross-domain communication

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .models import db
 import os
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) # Enable CORS for all routes
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///goaltrack.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
